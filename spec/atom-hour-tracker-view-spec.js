31d261e984d61f2093fa8980ef05dfbff03c56dd
Teemu Pöntelin <teemu.pontelin@reaktor.com>
2016-11-27 22:28:53 +0200

    Remove tests generated by the package generator

--- spec/atom-hour-tracker-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomHourTrackerView from '../lib/atom-hour-tracker-view';
-
-describe('AtomHourTrackerView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
